1bb8524c40102222c2891b4ad632f65b45cbba9f
Jack <jackgermainbusiness@gmail.com>
2025-08-15 14:19:01 +0100

    codewars 15/08/2025

---
+++ 8kyu/reverse-list-order.js
@@ -0,0 +1,12 @@
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// PREP
+// Parameters: Array of numbers
+// Returns: Array in reverse order
+// Example: [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// Psuedo:
+  // 1. return reverse array
+
+function reverseList(list) {
+  return list.reverse()
+}
