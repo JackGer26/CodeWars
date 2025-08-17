88a6421a0d11cc1b035b31fc15f095b7d55dd94b
Jack <jackgermainbusiness@gmail.com>
2025-08-17 10:00:34 +0100

    codewars 17/08/2025

---
+++ 8kyu/drink-about.js
@@ -0,0 +1,32 @@
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+
+// PREP
+// Params : integer
+// Return: "drink toddy", "drink coke", "drink beer", "drink whisky"
+// Example: 13 --> "drink toddy"
+// Psuedo code:
+  // 1. conditional statement for age
+  // 2. return appropriate string for age
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) {
+    return "drink toddy"
+  } else if (old < 18) {
+    return "drink coke"
+  } else if (old < 21) {
+    return "drink beer"
+  } else {
+    return "drink whisky"
+  }
+};
