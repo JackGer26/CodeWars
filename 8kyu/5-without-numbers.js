15cf17400b39d8196c94d539763de3c45a86604e
Jack <jackgermainbusiness@gmail.com>
2025-09-21 10:33:42 +0100

    coding challenge 21/09/2025

---
+++ 8kyu/5-without-numbers.js
@@ -0,0 +1,17 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+/*
+PREP
+
+Params: None
+Returns: 5
+Example: None
+Psuedo:
+1. Return length of 5 character string
+*/
+
+function unusualFive() {
+    return "done!".length
+  }
