1454a55d33fb44baf85a546957a845d7509edab0
Jack <jackgermainbusiness@gmail.com>
2025-09-22 12:11:05 +0100

    Coding challenge 22/09/2025

---
+++ 8kyu/combine-strings.js
@@ -0,0 +1,15 @@
+// Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+/*
+PREP
+
+Params: Two strings
+Returns: One string
+Example: With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+Psuedo:
+// Create function with name combineNames
+// Return parameters combined as one string
+*/
+
+let combineNames = (first, last) => `${first} ${last}`
+
