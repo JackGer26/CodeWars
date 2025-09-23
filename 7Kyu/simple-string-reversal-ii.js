94d27ce15543882d9e289cab59dbe5fc05ffda1d
Jack <jackgermainbusiness@gmail.com>
2025-09-23 10:27:07 +0100

    Coding challenge 23/09/2025

---
+++ 7Kyu/simple-string-reversal-ii.js
@@ -0,0 +1,29 @@
+// In this Kata, you will be given a string and two indexes (a and b). Your task is to reverse the portion of that string between those two indices inclusive.
+
+// str = "codewars", a = 1, b = 5 --> "cawedors"
+// str = "cODEWArs", a = 1, b = 5 --> "cAWEDOrs"
+// Input will be lowercase and uppercase letters only.
+
+// The first index a will always be smaller than the string length; the second index b can be greater than the string length. More examples in the test cases. Good luck!
+
+/*
+PREP
+
+Params:  String, Integer, Integer bigger than the last
+Returns: String, reversed between the index of the two integer parameters
+Example: str = "cODEWArs", a = 1, b = 5 --> "cAWEDOrs
+Psuedo:
+grab substring to be replaces
+reverse whole string
+replace substring with reversed string and join
+return string
+
+*/
+function solve(st, a, b){
+    let sub = st.substring(a, b+1)
+    let subReverse = sub.split('').reverse().join('')
+    
+    st = st.replace(sub, subReverse)
+     
+    return st
+   }
