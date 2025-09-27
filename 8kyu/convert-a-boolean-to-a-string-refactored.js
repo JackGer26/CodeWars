8ff0a7eb016035f436a611ac5c87e96f6933caab
Jack <jackgermainbusiness@gmail.com>
2025-09-27 13:09:35 +0100

    coding challenge 27/09/2025

---
+++ 8kyu/convert-a-boolean-to-a-string-refactored.js
@@ -0,0 +1,12 @@
+/*
+PREP
+
+Params: A boolean
+Return: A string of that boolean
+Example: True -> "True"
+Psuedo:
+if b === true, return "true"
+if b === false, return "false"
+*/
+
+let booleanToString = b => b === true ? "true" : "false"
