6605a31a5b690418cf66e143f921a279fab59fae
Jack <jackgermainbusiness@gmail.com>
2025-09-28 13:12:12 +0100

    coding challenge 28/09/2025

---
+++ 8kyu/convert-a-string-to-an-array-refactored.js
@@ -0,0 +1,11 @@
+/*
+PREP
+
+Params: A String
+Returns: The string as an array
+Example: "Robin Singh" ==> ["Robin", "Singh"]
+Psuedo:
+Split string
+*/
+
+let stringToArray = string => string.split(' ')
