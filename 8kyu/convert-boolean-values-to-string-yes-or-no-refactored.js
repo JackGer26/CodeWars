28ec82d0e70fe6bceb2a671c15ea6e196efd9586
Jack <jackgermainbusiness@gmail.com>
2025-09-30 13:25:31 +0100

    coding challenge 30/09/2025

---
+++ 8kyu/convert-boolean-values-to-string-yes-or-no-refactored.js
@@ -0,0 +1,14 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+/*
+PREP
+
+Params: Boolean
+Returns: "Yes" for true boolean or "No" for false boolean
+Example: false -> "No"
+Psuedo
+1. If bool is true, return yes
+2. if bool is false, return no
+*/
+
+let boolToWord = bool => bool === true ? "Yes" : "No"
