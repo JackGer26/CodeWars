2663a68c76f4acc0ededddf1a1280c264de6e04a
Jack <jackgermainbusiness@gmail.com>
2025-10-03 13:41:13 +0100

    coding challenge 03/10/2025

---
+++ 8kyu/convert-a-string-to-a-number-refactored.js
@@ -0,0 +1,16 @@
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+/* 
+PREP
+
+Params: A String
+Returns: The string as a number
+Example: "1234" --> 1234
+Psuedo:
+1. Return str as number
+
+*/
+
+let stringToNumber = str => Number(str)
