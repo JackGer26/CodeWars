e6a35a4020f7b39c7ef5eebf73149595e161fa09
Jack <jackgermainbusiness@gmail.com>
2025-10-04 13:50:44 +0100

    coding challenge 04/10/2025

---
+++ 8kyu/sum-the-strings-refactored.js
@@ -0,0 +1,14 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+/*
+PREP
+
+Params: Two numbers as strings
+Returns: The sum of the two numbers as a string
+Example: "4",  "5" --> "9" 
+Psuedo:
+1. Sum Number(a) and Number(b)
+2. Return sum as string
+*/
+
+let sumStr = (a, b) => (Number(a) + Number(b)).toString()
